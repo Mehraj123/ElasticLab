4873489d94f633e1b7d00791769062a94eb4d09d
mehraj.malik007@gmail.com <mehraj.malik007@gmail.com>
2019-09-27 12:52:46 +0530

    added elasticsearch version details

--- src/main/java/com/eslab/config/ElasticSearchConfig.java
+++ src/main/java/com/eslab/config/ElasticSearchConfig.java
@@ -1,6 +1,8 @@
 package com.eslab.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.Version;
+import org.elasticsearch.action.main.MainResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -9,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
 
 @Configuration
 public class ElasticSearchConfig {
@@ -26,6 +30,7 @@ public class ElasticSearchConfig {
         RestHighLevelClient highLevelClient = new RestHighLevelClient(
                 RestClient.builder(new HttpHost(host, port, HTTP_SCHEME)));
         checkESConnection(highLevelClient);
+
         return highLevelClient;
     }
 
@@ -34,9 +39,25 @@ public class ElasticSearchConfig {
             boolean isConnected = highLevelClient.ping(RequestOptions.DEFAULT);
             if (isConnected) {
                 logger.info("Connected to Elastic search engine..!");
+                printDetails(highLevelClient);
             }
         } catch (Exception e) {
-            logger.error("Failed to Connected to Elastic search engine.", e);
+            logger.error("Failed to Connected to Elastic search engine. /n" +
+                    "Shutting Down.", e);
+            System.exit(0);
         }
+    }
+
+    private void printDetails(RestHighLevelClient highLevelClient) {
+        try {
+            MainResponse response = highLevelClient.info(RequestOptions.DEFAULT);
+            String clusterName = response.getClusterName().value();
+            String nodeName = response.getNodeName();
+            Version version = response.getVersion();
+            logger.info(String.format("Version : %s\nCluster Name : %s\nNode Name : %s", version, clusterName, nodeName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
